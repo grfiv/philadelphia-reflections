bb8875da1a0917ed06a27663387667f85318f24d
George Fisher <george@georgefisher.com>
2016-10-16 16:38:04 -0400

    generic handheld-device index page; missing the forward link

--- public_html/indexhandheld-PDO.php
+++ public_html/indexhandheld-PDO.php
@@ -9,84 +9,50 @@
 
     include("inc/class_definitions.php");
     include("inc/pdo_database_connection.php");
+
+    $template_variables = array();
+
+    $copyright_end_year = date("Y");
+    $template_variables['copyright_end_year'] = $copyright_end_year;
+
+    $keys   = [484,485,1155,1101,565]; # array(...);
+    $qmarks = join(',', array_fill(0, count($keys), '?'));
+    $select = "SELECT title, description, table_key FROM individual_reflections WHERE table_key IN ($qmarks)";
+
+    $stmt = $pdo->prepare($select);
+    $stmt->execute($keys);
+    $blog_list = $stmt->fetchAll(PDO::FETCH_CLASS, 'blog');
+
+    $access_key = 1;
+
+    foreach ($blog_list as &$blog) {
+        $title       = $blog->title;
+        $table_key   = $blog->table_key;
+        $description = $blog->description;
+        $description = preg_replace('/<img .*?>/sim', '', $description); // strip out images
+        $description = preg_replace('/ class="firstDrop"/si', '', $description);
+        $description = preg_replace('/<!-- .*? -->/si', '', $description);
+        $description = preg_replace('%<table [^>]*?>.*?</table>%si', '', $description);
+        $description = preg_replace('/<img [^>]*?>/i', '', $description);
+
+        $blog->description = utf8_encode(trim($description));
+        $blog->access_key  = $access_key;
+        $blog->filename    = '#';
+
+        $access_key = $access_key + 1;
+    }
+
+    $template_variables['blog_list'] = $blog_list;
+
+    # call the template
+    # =================
+    require_once '../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('views');
+    $twig   = new Twig_Environment($loader, array(
+        // Uncomment the line below to cache compiled templates
+        // 'cache' => '/../cache',
+    ));
+
+    echo $twig->render('indexhandheld.twig', $template_variables);
+
 ?>
-<!DOCTYPE html>
-
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-
-        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-        <link rel="icon"          href="images/favicon.ico" type="image/x-icon" />
-
-        <!--                                      -->
-        <!-- PHILADELPHIA REFLECTIONS             -->
-        <!-- George R. Fisher III MD              -->
-        <!--                                      -->
-        <!-- (c) 2004 - <?php echo date("Y"); ?>                      -->
-        <!--                                      -->
-
-        <title>Philadelphia Reflections</title>
-
-        <style>
-            body {color:#000;background-color:#FFF;font-size:medium;margin:0;padding:0;}
-            #top > p           {font-family:garamond, times, serif;background-color:#22007D;color:#ea9f20;text-transform:uppercase;margin:0;padding:5px;}
-            #wrapper{width:100%;margin:0;padding:0;}#content{font-size:medium;color:#000;background-color:#FFF;margin:0;padding:10px;}
-            #content > p{text-align:left;}
-            .footerhandheld{font-family:garamond, times, serif;background-color:#22007D;}
-            .footerhandheld > p{color:#ea9f20;font-weight:700;text-align:center;margin:0 auto;padding:25px 10px;}
-            .links{font-size:2em;}.copyright{font-size:.90em;font-weight:400;color:#FFF;}#top,#center{margin:0;padding:0;}
-            .home,.email{color:#ea9f20;}
-        </style>
-    </head>
-
-    <body>
-
-      <div id="top">
-
-        <p>Philadelphia Reflections</p>
-
-      </div>
-
-      <div id="content">
-          <?php
-            $keys   = [484,485,1155,1101,565]; # array(...);
-            $qmarks = join(',', array_fill(0, count($keys), '?'));
-            $select = "SELECT * FROM individual_reflections WHERE table_key IN ($qmarks)";
-
-            $stmt = $pdo->prepare($select);
-            $stmt->execute($keys);
-
-            $access_key = 1;
-
-            while ($blog = $stmt->fetchObject('blog')) {
-                $title       = $blog->title;
-                $table_key   = $blog->table_key;
-                $description = $blog->description;
-                $description = preg_replace('/<img .*?>/sim', '', $description); // strip out images
-                $description = preg_replace('/ class="firstDrop"/si', '', $description);
-                $description = preg_replace('/<!-- .*? -->/si', '', $description);
-                $description = preg_replace('%<table [^>]*?>.*?</table>%si', '', $description);
-                $description = preg_replace('/<img [^>]*?>/i', '', $description);
-
-                // access keys suggested by http://www.ready.mobi
-                echo "<p><a accesskey='$access_key' href='$filename'>$title</a><br>\n";
-                $access_key = $access_key + 1;
-
-                echo utf8_encode(trim($description)) . "</p>\n\n";
-            };
-    ?>
-
-      </div>
-
-      <div class='footerhandheld'>
-        <p>
-            <a class='home' href='http://www.philadelphia-reflections.com'>HOME</a>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <a class='email' href='mailto:grfisheriii@gmail.com?subject=Message%20from%20a%20mobile%20device'>EMAIL</a><br>
-            <span style='color:white';>Copyright Dr. George Fisher 2004 - <?php echo date("Y"); ?>. All rights reserved.</span>
-        </p>
-      </div>
-
-    </body>
-</html>
